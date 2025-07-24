e801469c0c9d1401121bb6c3b646593cb238a768
Naman Muley <naman.g.muley@gmail.com>
2025-07-24 22:15:45 +0530

    Add src/App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import StatusDashboard from './components/StatusDashboard.jsx';
+
+const App = () => {
+  return (
+    <main>
+      <h1>Team Status Dashboard</h1>
+      <StatusDashboard />
+    </main>
+  );
+};
+
+export default App;
