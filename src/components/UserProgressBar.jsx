335a6d26520e8db1aa31c58894dd9d14cc4184ee
Naman Muley <naman.g.muley@gmail.com>
2025-07-24 22:15:52 +0530

    Add src/components/UserProgressBar.jsx

---
+++ src/components/UserProgressBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const UserProgressBar = ({ user, completed, total }) => {
+  // To be implemented: Accessible progressbar and labels
+  return (
+    <div className="user-progress">
+      {/* Progress bar UI */}
+    </div>
+  );
+};
+
+export default UserProgressBar;
